6de48c40496b323cf78e0b82284f40ba5ace3d39
virginiah894 <virgyperry@gmail.com>
2019-11-10 14:03:14 +0300

    fix all errors

--- user.py
+++ user.py
@@ -1,10 +1,10 @@
 class User:
     # import pyperclip
   
-        '''
-        A class blueprint that will intantiate users
-        '''
-        user_list=[]
+    '''
+    A class blueprint that will intantiate users
+    '''
+    user_list=[]
     
     def __init__(self,first_name,last_name,username,number,email,password):
 
@@ -19,31 +19,32 @@ class User:
         self.password = password
     def saved_user(self):
         User.user_list.append(self)
+    
     def remove_user(self):
         '''
         method for removing a user from the user list
         '''
         User.user_list.remove(self)
-
 class Credentials:
-        '''
-        A class for generating the username and password
-        '''
-        credentials_list[]
-        '''
-        An empty list that will be a blueprint for credentials
-        '''
+    '''
+    A class for generating the username and password
+    '''
+    credentials_list=[]
+    '''
+    An empty list that will be a blueprint for credentials
+    '''
     # User_credentials 
-    def __init__(username,accounts,password):
+    def __init__(self,username,accounts,password):
+        
         '''
         An blueprint for the credentials class
         '''
         self.username = username
-        self.accounts = acounts
+        self.accounts = accounts
         self.password = password
     def saved_credential(self):
-        Credential.credentials_list.append(self)
-    def remove_user(self):
+        Credentials.credentials_list.append(self)
+    def remove_credential(self):
         '''
         method for removing a user from the user list
         '''
@@ -51,28 +52,28 @@ class Credentials:
 
     
 
+    # @classmethod
+    # def find_by_username(cls,username):
+    #     '''
+    #     method for searching by using the username to return the details
+    #     '''
+    #     for credentials in cls.credentials_list:
+    #         if credentials.username == username:
+    #             return username
     @classmethod
-    def find_by_username(cls,username):
-        '''
-        method for searching by using the username to return the details
-        '''
-        for user in cls.user_list:
-            if user.username == username:
-                return user
-    @classmethod
-    def user_inlist(cls,username):
+    def credentials_inlist(cls,username):
         '''Method for confirming if the username exists.the method'''
         '''
         uses username as argument to return a true or false answer
         '''
-        for user in cls.user_list:
-            if user.username == username:
+        for credentials in cls.credentials_list:
+            if credentials.username == username:
                 return True
         return False
     @classmethod
     def list_all(cls):
-        return cls.user_list
-    @classmethod
-    def copy_details(cls,username):
-        user_details = User.find_by_username(username)
-        pyperclip.copy(user_details.first_name.last_name.username .number.email.password)
+        return cls.credentials_list
+    # @classmethod
+    # def copy_details(cls,username):
+    #     user_details = User.find_by_username(username)
+    #     pyperclip.copy(user_details.first_name.last_name.username .number.email.password)
